f69a05d9b6ab4fc8e645acce06f90d6a24b5a335
Coleman <gibsonjc@rose-hulman.edu>
2017-04-19 17:42:54 -0400

    Added a basic demonstration of a login form
    It is not really functional in any way, but it
    does demonstrate a few react js concepts and
    is at least a start

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,31 @@
 
+
+/*
+   Imports
+*/
+
+// libraries
 import React from 'react';
+
+// other components
+import LoginForm from './Login/LoginForm.jsx'
+
+// css
+
+
+/*
+   Code
+*/
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello, world!!!</h1>
+                <h1>Dating for Nerds</h1>
+
+                <LoginForm>
+                </LoginForm>
             </div>
         );
     }
 }
-
---
+++ client/src/components/Login/LoginForm.jsx
@@ -0,0 +1,62 @@
+
+/*
+   Imports
+*/
+
+import React from 'react';
+
+export default class LoginForm extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+
+    console.log('Constructor called');
+
+    this.state = {
+      usernameStarted: false,
+      username: ''
+    }
+
+  }
+
+
+  usernameChanged(event) {
+    this.setState({usernameStarted: true});
+    this.setState({username: event.target.value});
+  }
+
+
+  isValidUsername(uname) {
+    return /\w+/.test(uname) || !this.state.usernameStarted;
+  }
+
+
+
+  render() {
+
+    let invalidUsername;
+
+    if (!this.isValidUsername(this.state.username)) {
+      invalidUsername = <label>Invalid username.</label>
+    }
+
+    return (
+      <form>
+
+        <label htmlFor="username-input">Username:</label>
+        <input id="username-input"
+               type="text"
+               value={this.state.username}
+               onChange={this.usernameChanged.bind(this)}/>
+        {invalidUsername}
+
+        <p>{this.state.username}</p>
+
+        <button type="button">Create profile</button>
+      </form>
+    );
+  }
+
+
+}
