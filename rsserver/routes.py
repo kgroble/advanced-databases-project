c65934e21466bea99c802debc0eebbcd1fa8184e
Coleman <gibsonjc@rose-hulman.edu>
2017-04-20 17:40:29 -0400

    Changed to default route

--- rsserver/routes.py
+++ rsserver/routes.py
@@ -6,19 +6,21 @@ app = Flask(__name__, static_url_path='/static', static_folder='../client/dist/'
 
 uname = "No username provided"
 
-@app.route('/')
-def hello_world():
-    return send_from_directory('../client/src/', 'index.html')
-
 @app.route('/users/', methods=['GET', 'POST'])
 def users():
 	global uname
 	if (request.method == 'POST'):
 		uname = request.form['username']
 		# do something with this
-		
+
 	if (request.method == 'GET'):
 		return uname
+
+
+@app.route('/<path:path>')
+def hello_world(path):
+    return send_from_directory('../client/src/', 'index.html')
+
 
 if (__name__ == '__main__'):
 	app.run(debug=True)
