40e45f1d7781dcbf516f68ad547c9a35a8c4c3e6
Coleman <gibsonjc@rose-hulman.edu>
2017-04-20 16:33:14 -0400

    Can now request our client pages from flask

--- rsserver/routes.py
+++ rsserver/routes.py
@@ -1,13 +1,13 @@
-from flask import Flask, abort, redirect, url_for
-app = Flask(__name__)
+
+from flask import Flask, abort, redirect, url_for, render_template, \
+        send_from_directory
+
+app = Flask(__name__, static_url_path='/static', static_folder='../client/dist/')
 
 @app.route('/')
 def hello_world():
-	return 'Hello world!'
+    return send_from_directory('../client/src/', 'index.html')
 
-@app.route('/anotherpage/', methods=['GET', 'POST'])
-def another_page():
-
-	
-	
-	return 'Another page!'
+# @app.route('/login/', methods=['GET', 'POST'])
+# def another_page():
+#     return 'Another page!'
