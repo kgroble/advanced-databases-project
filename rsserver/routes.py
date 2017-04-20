5f1f82de28de85b178225592bc9ed58f2eeb76f9
kuhnerdm <kuhnerdm@rose-hulman.edu>
2017-04-20 16:49:13 -0400

    Added primitive username logic thing

--- rsserver/routes.py
+++ rsserver/routes.py
@@ -1,13 +1,24 @@
 
 from flask import Flask, abort, redirect, url_for, render_template, \
-        send_from_directory
+        send_from_directory, request
 
 app = Flask(__name__, static_url_path='/static', static_folder='../client/dist/')
+
+uname = "No username provided"
 
 @app.route('/')
 def hello_world():
     return send_from_directory('../client/src/', 'index.html')
 
-# @app.route('/login/', methods=['GET', 'POST'])
-# def another_page():
-#     return 'Another page!'
+@app.route('/users/', methods=['GET', 'POST'])
+def users():
+	global uname
+	if (request.method == 'POST'):
+		uname = request.form['username']
+		# do something with this
+		
+	if (request.method == 'GET'):
+		return uname
+
+if (__name__ == '__main__'):
+	app.run(debug=True)
