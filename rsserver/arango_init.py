609334289e517174c6e3cd9c59398c5c939f24e4
Coleman <gibsonjc@rose-hulman.edu>
2017-04-26 00:13:03 -0400

    Added database, index creation to arango_init

--- rsserver/arango_init.py
+++ rsserver/arango_init.py
@@ -3,21 +3,23 @@ from pyArango.graph import Graph, EdgeDefinition
 from pyArango.collection import Collection, Field
 from pyArango.collection import Edges
 
-conn = Connection(arangoURL='http://127.0.0.1:8530', username='root', password='foobar')
+conn = Connection(arangoURL='http://127.0.0.1:8530',
+                  username='root',
+                  password='foobar')
 
+# Creating/getting the database
 try:
     db = conn['RelationalSchema']
 except KeyError:
     conn.createDatabase(name = 'RelationalSchema')
     db = conn['RelationalSchema']
 
+# Weird arango driver stuff
 class Users(Collection):
     _fields = {'uname': Field()}
 
-
 class Match(Edges):
     _fields = {'strength': Field()}
-
 
 class UserGraph(Graph):
     _edgeDefinitions = [EdgeDefinition('Match',
@@ -25,11 +27,15 @@ class UserGraph(Graph):
                                        toCollections = ['Users'])]
     _orphanedCollections = []
 
-
+# Creating collections
 db.createCollection('Users')
 db.createCollection('Match')
-g = db.createGraph('UserGraph')
 
+# Creating indexes
+db['Users'].ensureHashIndex(['uname'], unique = True)
+
+
+g = db.createGraph('UserGraph')
 c = g.createVertex('Users', {'uname': 'coleman'})
 k = g.createVertex('Users', {'uname': 'kieran'})
 g.link('Match', c, k, {'strength': 9999})
